97e1ded36c10e7ba71fd488e195bc16f3a841c25
Ericgacoki <gacokieric@gmail.com>
2022-07-30 17:40:15 +0300

    DI module

---
+++ app/src/main/java/com/ericg/paging3xml/di/AppModule.kt
@@ -0,0 +1,55 @@
+package com.ericg.paging3xml.di
+
+import com.ericg.paging3xml.data.remote.ApiService
+import com.ericg.paging3xml.data.repositoryimpl.RickMortyRepositoryImpl
+import com.ericg.paging3xml.domain.repository.RickMortyRepository
+import com.ericg.paging3xml.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesAPIService(okHttpClient: OkHttpClient): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesRickMortyRepository(apiService: ApiService): RickMortyRepository {
+        return RickMortyRepositoryImpl(apiService)
+    }
+}
