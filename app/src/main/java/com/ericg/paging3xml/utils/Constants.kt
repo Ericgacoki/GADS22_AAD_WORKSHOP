b580facd341d83e5cc3d3246e5012d8e2beddeda
Ericgacoki <gacokieric@gmail.com>
2022-07-30 17:41:59 +0300

    Constants

---
+++ app/src/main/java/com/ericg/paging3xml/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.ericg.paging3xml.utils
+
+object Constants {
+    const val BASE_URL = "https://rickandmortyapi.com/api/"
+    const val STARTING_PAGE = 1
+    const val NETWORK_PAGE_SIZE = 50
+}
