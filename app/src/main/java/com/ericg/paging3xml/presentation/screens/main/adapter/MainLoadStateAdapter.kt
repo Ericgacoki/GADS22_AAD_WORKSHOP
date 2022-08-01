368f5390118cb848a8f42599a153c40c9548db50
Ericgacoki <gacokieric@gmail.com>
2022-08-02 00:08:19 +0300

    add retry button

--- app/src/main/java/com/ericg/paging3xml/presentation/screens/main/adapter/MainLoadStateAdapter.kt
+++ app/src/main/java/com/ericg/paging3xml/presentation/screens/main/adapter/MainLoadStateAdapter.kt
@@ -8,19 +8,38 @@ import androidx.paging.LoadStateAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.ericg.paging3xml.databinding.LoadStateViewBinding
 
-class MainLoadStateAdapter : LoadStateAdapter<MainLoadStateAdapter.LoadStateViewHolder>() {
-    inner class LoadStateViewHolder(val binding: LoadStateViewBinding) :
-        RecyclerView.ViewHolder(binding.root)
+class MainLoadStateAdapter(
+    private val retry: () -> Unit
+) : LoadStateAdapter<MainLoadStateAdapter.LoadStateViewHolder>() {
+
+    inner class LoadStateViewHolder(
+        val binding: LoadStateViewBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun setRetry() {
+            binding.also {
+                it.btnRetry.setOnClickListener {
+                    retry()
+                }
+            }
+        }
+    }
 
     override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
         holder.binding.apply {
             progress.isVisible = loadState is LoadState.Loading
+            tvErrorMsg.isVisible = loadState is LoadState.Error
+            btnRetry.isVisible = loadState is LoadState.Error
+
+            if (loadState is LoadState.Error) {
+                tvErrorMsg.text = loadState.error.localizedMessage
+            }
         }
+        holder.setRetry()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): LoadStateViewHolder {
         return LoadStateViewHolder(
-            LoadStateViewBinding.inflate(
+            binding = LoadStateViewBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
             )
         )
