e902e18d2c39e6d6762ad478effdd5acb640852c
Ericgacoki <gacokieric@gmail.com>
2022-08-31 23:52:43 +0300

    update

--- app/src/main/java/com/ericg/paging3xml/domain/repository/RickMortyRepository.kt
+++ app/src/main/java/com/ericg/paging3xml/domain/repository/RickMortyRepository.kt
@@ -1,13 +1,10 @@
 package com.ericg.paging3xml.domain.repository
 
-import androidx.paging.PagingData
-import com.ericg.paging3xml.data.pagingdatasource.RickMortyDataSource
-import com.ericg.paging3xml.data.remote.ApiService
-import com.ericg.paging3xml.domain.model.Character
-import com.ericg.paging3xml.utils.Resource
-import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
+import androidx.paging.PagingSource
+import com.ericg.paging3xml.data.local.entity.CharacterEntity
+import com.ericg.paging3xml.data.pagingdatasource.RickMortyRemoteMediator
 
-interface RickMortyRepository  {
-    fun getCharacters(): RickMortyDataSource
+interface RickMortyRepository {
+    fun getRickMortyDataSource(): PagingSource<Int, CharacterEntity>
+    fun getRickMortyRemoteMediator(): RickMortyRemoteMediator
 }
